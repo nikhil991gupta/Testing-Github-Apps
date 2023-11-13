a65cc20bf24d48b0a9d2064a69ff4945d6e749c7
Nikhil Gupta <66847019+nikhil991gupta@users.noreply.github.com>
2023-11-13 23:44:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log(hi);
+
+function isPalindrome(str) {
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  return cleanStr === cleanStr.split('').reverse().join('');
+}
+
+const testString = "A man, a plan, a canal, Panama!";
+if (isPalindrome(testString)) {
+  console.log(`${testString} is a palindrome.`);
+} else {
+  console.log(`${testString} is not a palindrome.`);
+}
